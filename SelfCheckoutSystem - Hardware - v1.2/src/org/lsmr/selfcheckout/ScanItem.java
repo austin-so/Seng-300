bf929c1cdf7a2db65c9ad6ff2752cd57f17e8cab
hasan-na <60748908+hasan-na@users.noreply.github.com>
2021-03-15 22:43:35 -0400

    Update ScanItem.java

--- SelfCheckoutSystem - Hardware - v1.2/src/org/lsmr/selfcheckout/ScanItem.java
+++ SelfCheckoutSystem - Hardware - v1.2/src/org/lsmr/selfcheckout/ScanItem.java
@@ -1,5 +1,37 @@
 package org.lsmr.selfcheckout;
 
+import java.util.ArrayList;
+
+import org.lsmr.selfcheckout.devices.BarcodeScanner;
+
+/**
+ * 
+ * Scan an item that has a barcode
+ *
+ */
 public class ScanItem {
+private BarcodedItem item;
+	private ArrayList<Barcode> registerItemCode; 
+	
+	/**
+	 * 
+	 * @param item
+	 * 			the item that the costumer wants to scan
+	 */
+	public ScanItem(BarcodedItem item) {
+		BarcodeScanner bars = new BarcodeScanner();
+		bars.scan(item);
+		
+		this.item = item;
+	}
+	
+	/**
+	 * Store the items barcode into an arraylist
+	 */
+	public void storeItemCode() {
+		Barcode itemcode = item.getBarcode(); 
+		registerItemCode.add(itemcode);
+	}
+}
 
 }
