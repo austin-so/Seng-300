7c3905630612bb7be7d5b1887c2ed2dfd554e974
hasan-na <60748908+hasan-na@users.noreply.github.com>
2021-03-16 21:50:24 -0400

    Update PayBankNote.java

--- SelfCheckoutSystem - Hardware - v1.2/src/org/lsmr/selfcheckout/PayBankNote.java
+++ SelfCheckoutSystem - Hardware - v1.2/src/org/lsmr/selfcheckout/PayBankNote.java
@@ -1,5 +1,60 @@
 package org.lsmr.selfcheckout;
 
+import java.util.Currency;
+import java.util.Locale;
+
+import org.lsmr.selfcheckout.devices.BanknoteSlot;
+import org.lsmr.selfcheckout.devices.BanknoteStorageUnit;
+import org.lsmr.selfcheckout.devices.BanknoteValidator;
+import org.lsmr.selfcheckout.devices.BidirectionalChannel;
+import org.lsmr.selfcheckout.devices.DisabledException;
+import org.lsmr.selfcheckout.devices.OverloadException;
+import org.lsmr.selfcheckout.devices.UnidirectionalChannel;
+
+
 public class PayBankNote {
+ private BanknoteStorageUnit capacity = new BanknoteStorageUnit(500);
+	private Banknote banknote = new Banknote(0, null);
+	
+	/**
+	 * 
+	 * @param banknote
+	 * 			Allows the user to pay for their items with banknotes
+	 * 
+	 * @throws DisabledException
+	 * 			If the storageunit is disabled
+	 * 
+	 * @throws OverloadException
+	 * 			If the storageunit is full
+	 */			
+	public PayBankNote(Banknote banknote) throws DisabledException, OverloadException {
+		this.banknote = banknote;
+		
+		int[] denomination = {5,10,20,50,100};
+		
+		Currency cur = Currency.getInstance(Locale.CANADA);
+		
+		BanknoteValidator valid = new BanknoteValidator(cur, denomination);
+		BanknoteSlot banknoteSlot = new BanknoteSlot(false);
+		BidirectionalChannel<Banknote> ejection = new BidirectionalChannel<Banknote>(banknoteSlot, valid);
+		UnidirectionalChannel<Banknote> accepted = new UnidirectionalChannel<Banknote>(valid);
+		
+		valid.connect(ejection, accepted);
+		valid.accept(banknote);
+		checkFullStorage();
+	}
+	
+	/**
+	 * 		Method to check if the storageunit is full
+	 * @throws DisabledException
+	 * 		If the storageunit is disabled
+	 * @throws OverloadException
+	 * 		If the storageunit is full
+	 */
+	public void checkFullStorage() throws DisabledException, OverloadException {
+		capacity.accept(banknote);
+	}
 
 }
+
+
