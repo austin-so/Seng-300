aaa332e128feab259b372142d45fc8dfb88be9e0
austin-so <austin1997416@gmail.com>
2021-03-17 00:18:15 -0600

    Create Bagging.java

---
+++ SelfCheckoutSystem - Hardware - v1.2/src/org/lsmr/selfcheckout/Bagging.java
@@ -0,0 +1,26 @@
+package org.lsmr.selfcheckout;
+
+import java.util.Arrays;
+import org.lsmr.selfcheckout.Item;
+import org.lsmr.selfcheckout.devices.ElectronicScale;
+
+public class Bagging {
+
+	/*
+	 * get latest scanned item and related weight
+	 */
+	
+	public int itemWeight() {
+		ScanItem items = new ScanItem();
+		
+		return 0;
+		
+	}
+	/*
+	 * if scale detect added weight +- sensitivity, signal
+	 */
+	
+	/*
+	 * if scale detect remove, throw exception
+	 */
+}
