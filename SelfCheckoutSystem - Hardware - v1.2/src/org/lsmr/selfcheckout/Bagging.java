0bfd6141aca700491464269f42f6576841099cac
austin-so <austin1997416@gmail.com>
2021-03-17 16:36:24 -0600

    Update Bagging.java

--- SelfCheckoutSystem - Hardware - v1.2/src/org/lsmr/selfcheckout/Bagging.java
+++ SelfCheckoutSystem - Hardware - v1.2/src/org/lsmr/selfcheckout/Bagging.java
@@ -1,5 +1,6 @@
 package org.lsmr.selfcheckout;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import org.lsmr.selfcheckout.Item;
 import org.lsmr.selfcheckout.devices.ElectronicScale;
@@ -11,19 +12,52 @@ public class Bagging {
 	/*
 	 * get latest scanned item and related weight
 	 */
-	private Array[] itemWeight;
+	private double[] itemWeight;
+	private BarcodedItem item;
 	
+	/**
+	 * @param item 
+	 * 		The item  that was first scanned 
+	 */
+	public Bagging(BarcodedItem item) {
+		this.item = item;
+	}
+	
+	/**
+	 * @param itemWeight 
+	 * 		The array of item weight, without any new item scanned
+	 */
+	public Bagging(double[] itemWeight) {
+		this.itemWeight = itemWeight;
+	}
+	
+	/**
+	 * @param itemWeight 
+	 * 		The array of item weight, without any new item scanned
+	 */
+	public Bagging(double[] itemWeight, BarcodedItem item) {
+		this.itemWeight = itemWeight;
+		this.item = item;
+	}
 	public void getItemWeight() {
 		
-		ScanItem scannedItem = new ScanItem();
-		Array[] temp = scannedItem.getStoredItem();
+		ScanItem scannedItem = new ScanItem(item);
+		ArrayList<BarcodedItem> temp = scannedItem .getStoreItem();
 		
-		for(int x=0; x<itemWeight.length; x++) {
-			BarcodedItem i = temp[x];
+		for(int x=0; x<temp.size(); x++) {
+			BarcodedItem i = temp.get(x);
 			double Weight = i.getWeight();
 			itemWeight[x] = Weight;
 		}
 		
+	}
+	
+	public int getTotalWeight() {
+		int totalWeight = 0;
+		for(int x=0; x<itemWeight.length; x++) {
+			totalWeight += itemWeight[x];
+		}
+		return totalWeight; 
 	}
 	
 	/*
@@ -34,7 +68,7 @@ public class Bagging {
 	 * if scale detect remove, throw exception
 	 */
 	public void baggingProcess() {
-
+		
 		
 	}
 }
